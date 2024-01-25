0d84121ddc3729911271007499d60e9263680d06
rahul <rahulprajapati855.rp@gmail.com>
2024-01-25 11:42:30 +0530

    first commit

---
+++ src/test/java/com/staragile/assignment/sprintbootdemo/SprintbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staragile.assignment.sprintbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
